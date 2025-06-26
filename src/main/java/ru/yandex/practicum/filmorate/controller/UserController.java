9964500de77f652cbab44e075f568a8416b53b2c
RomanUpyr <ru999626@yandex.ru>
2025-06-26 21:25:45 +0800

    Исправил замечания

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.*;
 import jakarta.validation.Valid;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
@@ -26,28 +27,26 @@ public class UserController {
 
     @PostMapping // Обрабатывает HTTP POST запросы по пути /users
     public User createUser(@Valid @RequestBody User user) {
-        log.info("Получен запрос на создание пользователя: {}", user);
-        if (user.getName() == null || user.getName().isBlank()) {
+        log.debug("Получен запрос на создание пользователя: {}", user);
+        if (!StringUtils.hasText(user.getName())) {
             user.setName(user.getLogin());
         }
-        validateUser(user); // Валидация пользователь согласно задания
         user.setId(getNextId()); // Устанавливаем новый ID перед сохранением
         users.put(user.getId(), user);// Сохраняем пользователя в Map
-        log.info("Пользователь успешно создан: {}", user);
+        log.debug("Пользователь успешно создан: {}", user);
         return user; // Возвращаем созданного пользователя
     }
 
     @PutMapping // Обрабатывает HTTP PUT запросы по пути /users
     public User updateUser(@Valid @RequestBody User user) {
-        log.info("Получен запрос на обновление пользователя с ID {}: {}", user.getId(), user);
-        validateUser(user); // Валидация пользователь согласно задания
+        log.debug("Получен запрос на обновление пользователя с ID {}: {}", user.getId(), user);
         // Проверяем существование пользователя
         if (!users.containsKey(user.getId())) {
             throw new ValidationException("Пользователь с id=" + user.getId() + " не найден");
         }
         // Обновляем данные пользователя
         users.put(user.getId(), user);
-        log.info("Пользователь успешно обновлён: {}", user);
+        log.debug("Пользователь успешно обновлён: {}", user);
         // Возвращаем обновленного пользователя
         return user;
     }
@@ -60,12 +59,4 @@ public class UserController {
                 .max()
                 .orElse(0) + 1;
     }
-
-    // Метод валидации пользователя согласно задания
-    private void validateUser(User user) {
-        if (user.getBirthday().isAfter(LocalDate.now())) {
-            throw new ValidationException("Дата рождения не может быть в будущем");
-        }
-    }
-
 }
