0bf99da796ec89a0474a4bdb87b1ade6a7dd73e8
RomanUpyr <ru999626@yandex.ru>
2025-08-05 13:20:52 +0800

    Поправил код

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -64,5 +64,5 @@ MERGE INTO friendship (user_id, friend_id, status_id) VALUES
 (2, 4, 1);  -- Мария отправила заявку Елене (неподтверждённая)
 
 -- Сброс sequence для автоинкремента (чтобы новые записи начинались с правильного ID)
--- ALTER TABLE users ALTER COLUMN id RESTART WITH 6;
--- ALTER TABLE films ALTER COLUMN id RESTART WITH 7;
+ALTER TABLE users ALTER COLUMN id RESTART WITH 16;
+ALTER TABLE films ALTER COLUMN id RESTART WITH 17;
